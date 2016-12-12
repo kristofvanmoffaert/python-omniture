16d8844eeb04bd0c0069cb9469dd188ed61ac692
kristofvanmoffaert <kristof.van.moffaert@gmail.com>
2016-12-12 09:38:10 +0100

    Update account.py

--- omniture/account.py
+++ omniture/account.py
@@ -22,7 +22,7 @@ class Account(object):
     """ A wrapper for the Adobe Analytics API. Allows you to query the reporting API """
     DEFAULT_ENDPOINT = 'https://api.omniture.com/admin/1.4/rest/'
 
-    def __init__(self, username, secret, endpoint=DEFAULT_ENDPOINT, cache=False, cache_key=None, proxies=None, timeout=0.001):
+    def __init__(self, username, secret, endpoint=DEFAULT_ENDPOINT, cache=False, cache_key=None, proxies=None, timeout=10):
         """Authentication to make requests."""
         self.log = logging.getLogger(__name__)
         self.log.info(datetime.now().strftime("%Y-%m-%d %I%p:%M:%S"))
@@ -30,10 +30,10 @@ class Account(object):
         self.secret = secret
         self.endpoint = endpoint
         #Allow someone to set a custom cache key
-        self.cache = cache
-		self.timeout = timeout
+        self.timeout = timeout
         if proxies:
             self.proxies= proxies
+        self.cache = cache
         if cache_key:
             self.cache_key = cache_key
         else:
@@ -89,16 +89,16 @@ class Account(object):
             like to pass to the API
         """
         self.log.info("Request: %s.%s  Parameters: %s", api, method, query)
-		if self.proxies:
+        if self.proxies:
 			response = requests.post(
-            self.endpoint,
-            params={'method': api + '.' + method},
-            data=json.dumps(query),
-            headers=self._build_token(),
-            proxies = self.proxies,
-			verify=False,
-			timeout = self.timeout
-            )
+                self.endpoint,
+                params={'method': api + '.' + method},
+                data=json.dumps(query),
+                headers=self._build_token(),
+                proxies = self.proxies,
+                verify=False,
+                timeout = self.timeout
+                )
         else:
 			response = requests.post(
 				self.endpoint,
